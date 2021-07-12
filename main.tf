c3de0a8fe76b199e003ece23882cd27fbc137c1c
yaswanth seela <57526711+yaswanth8008@users.noreply.github.com>
2021-07-12 17:26:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,73 +1,66 @@
-## <https://www.terraform.io/docs/providers/azurerm/index.html>
-provider "azurerm" {
-  version = "=2.5.0"
-  features {}
-}
+provider   "azurerm"   { 
+   version   =   "= 2.0.0" 
+   features   {} 
+ } 
+# Azure Resource Group
+ resource   "azurerm_resource_group"   "rg"   { 
+   name   =   "my-first-terraform-rg" 
+   location   =   "northeurope" 
+ } 
+#Virtual Network and Subnet
+ resource   "azurerm_virtual_network"   "myvnet"   { 
+   name   =   "my-vnet" 
+   address_space   =   [ "10.0.0.0/16" ] 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+ } 
 
-## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
-resource "azurerm_resource_group" "rg" {
-  name     = "TerraformDemo"
-  location = "eastus"
-}
+ resource   "azurerm_subnet"   "frontendsubnet"   { 
+   name   =   "frontendSubnet" 
+   resource_group_name   =    azurerm_resource_group.rg.name 
+   virtual_network_name   =   azurerm_virtual_network.myvnet.name 
+   address_prefix   =   "10.0.1.0/24" 
+ } 
+#New Public IP Address
+ resource   "azurerm_public_ip"   "myvm1publicip"   { 
+   name   =   "pip1" 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
+   allocation_method   =   "Dynamic" 
+   sku   =   "Basic" 
+ } 
+#Network Interface for our VM
+ resource   "azurerm_network_interface"   "myvm1nic"   { 
+   name   =   "myvm1-nic" 
+   location   =   "northeurope" 
+   resource_group_name   =   azurerm_resource_group.rg.name 
 
-## <https://www.terraform.io/docs/providers/azurerm/r/availability_set.html>
-resource "azurerm_availability_set" "DemoAset" {
-  name                = "example-aset"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+   ip_configuration   { 
+     name   =   "ipconfig1" 
+     subnet_id   =   azurerm_subnet.frontendsubnet.id 
+     private_ip_address_allocation   =   "Dynamic" 
+     public_ip_address_id   =   azurerm_public_ip.myvm1publicip.id 
+   } 
+ } 
+#Virtual Machine
+ resource   "azurerm_windows_virtual_machine"   "example"   { 
+   name                    =   "myvm1"   
+   location                =   "northeurope" 
+   resource_group_name     =   azurerm_resource_group.rg.name 
+   network_interface_ids   =   [ azurerm_network_interface.myvm1nic.id ] 
+   size                    =   "Standard_B1s" 
+   admin_username          =   "adminuser" 
+   admin_password          =   "Password123!" 
 
-## <https://www.terraform.io/docs/providers/azurerm/r/virtual_network.html>
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vNet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+   source_image_reference   { 
+     publisher   =   "MicrosoftWindowsServer" 
+     offer       =   "WindowsServer" 
+     sku         =   "2019-Datacenter" 
+     version     =   "latest" 
+   } 
 
-## <https://www.terraform.io/docs/providers/azurerm/r/subnet.html> 
-resource "azurerm_subnet" "subnet" {
-  name                 = "internal"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "10.0.2.0/24"
-}
-
-## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
-resource "azurerm_windows_virtual_machine" "example" {
-  name                = "Demo VM"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = "Standard_F2"
-  admin_username      = "adminuser"
-  admin_password      = "Diebold#1234"
-  availability_set_id = azurerm_availability_set.DemoAset.id
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
+   os_disk   { 
+     caching             =   "ReadWrite" 
+     storage_account_type   =   "Standard_LRS" 
+   } 
+ }
