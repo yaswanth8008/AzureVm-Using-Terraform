d222845247a0d20ce7d93405452734c35b94a1a5
yaswanth seela <57526711+yaswanth8008@users.noreply.github.com>
2021-07-12 07:23:31 +0000

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,73 @@
+## <https://www.terraform.io/docs/providers/azurerm/index.html>
+provider "azurerm" {
+  version = "=2.5.0"
+  features {}
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/resource_group.html>
+resource "azurerm_resource_group" "rg" {
+  name     = "TerraformDemo"
+  location = "eastus"
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/availability_set.html>
+resource "azurerm_availability_set" "DemoAset" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/virtual_network.html>
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/subnet.html> 
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "10.0.2.0/24"
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/network_interface.html>
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+## <https://www.terraform.io/docs/providers/azurerm/r/windows_virtual_machine.html>
+resource "azurerm_windows_virtual_machine" "example" {
+  name                = "Demo VM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "Diebold#1234"
+  availability_set_id = azurerm_availability_set.DemoAset.id
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "client_id" {
+    description =   "6ac4dc3b-e4a3-43b5-8045-3e00bc716ad3"
+    type        =   string
+}
+
+variable "client_secret" {
+    description =   "Fr43MiT3gtlcHSgRmA3qvoStSv.n_sQfkW"
+    type        =   string
+}
+
+variable "subscription_id" {
+    description =   "ca292399-122d-4603-8f0f-749ec5b551cb"
+    type        =   string
+}
+
+variable "tenant_id" {
+    description =   "6865e58b-8b01-47b2-952a-d164ab608852"
+    type        =   string
+}
